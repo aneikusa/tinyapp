f7c6fe0cab88f69e384e2cd8d1c106f8e6bca978
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-09-30 03:39:19 -0400

    Redirect requests to longURL

--- express_server.js
+++ express_server.js
@@ -45,9 +45,15 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
 });
 
 app.listen(PORT, () => {
