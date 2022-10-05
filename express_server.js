fea31317e59fe4f1a9d2488011bce2cd6bd16d69
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-10-04 20:10:06 -0400

    Pass user object to header

--- express_server.js
+++ express_server.js
@@ -8,11 +8,11 @@ app.set("view engine", "ejs"); // setting ejs as view engine
 app.use(express.urlencoded({ extended: true })); // parse the body
 app.use(cookieParser());
 
-function generateRandomString() {
+function generateRandomString () {
   return Math.random().toString(36).slice(2);
-} 
-// console.log(generateRandomString()); 
+} // console.log(generateRandomString()); 
 
+const users = {};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,20 +33,22 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"],
     urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  // const templateVars = {username: req.cookies["username"]};
-  res.render("urls_new");
+  const templateVars = {
+    user_id: req.cookies["user_id"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"],
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
@@ -60,7 +62,7 @@ app.get("/u/:id", (req, res) => {
 
 app.get("/register", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"],
   }
   res.render("registration", templateVars);
 })
@@ -83,14 +85,28 @@ app.post("/urls/:id/update", (req,res)=>{
   });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("user_id", req.body.user_id);
   res.redirect("/urls");
 }); // login
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 }); // logout
+
+app.post("/register", (req, res) => {
+const userID = generateRandomString();
+const userEmail = req.body.email;
+const userPassword = req.body.password;
+  users[userID] = {
+    id: userID,
+    email: userEmail,
+    password: userPassword
+  }
+
+res.cookie("user_id",users[userID]);
+res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
